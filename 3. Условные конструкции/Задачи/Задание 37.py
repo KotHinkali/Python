a2aad40c0ee35df75057baed465998b0c9172740
Анастасия <anderesprostokriss@gmail.com>
2026-09-22 17:08:40 +0300

    Решение задач

--- 3. Условные конструкции/Задачи/Задание 37.py
+++ 3. Условные конструкции/Задачи/Задание 37.py
@@ -1,7 +1,7 @@
 a = int(input())
 if a % 10 == 1 and a % 100 != 11:
     print("гриб")
-elif 2 <= a % 10 <= 4 and not (a % 100 <= 12 and a % 100 <= 14):
+elif 2 <= a % 10 <= 4 and not(12<=a%100<=15):
     print("гриба")
 else:
     print("грибов")
--- 4. Циклы/4.2 Цикл while/Задачи/Задание 413.py
+++ 4. Циклы/4.2 Цикл while/Задачи/Задание 413.py
@@ -1,1 +1,10 @@
-
+a=10
+b=[]
+c=0
+while a!=0:
+    a=int(input())
+    b.append (a)
+for i in range(len(b)):
+    if b[i]>c:
+        c=index.b[i]
+    if b[i]>c and b[i]<
